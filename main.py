8ad73c5e946cc6879cacf487d734e540bffc7c41
Joshua <132035532+Kailenator-AZH@users.noreply.github.com>
2024-04-05 09:09:03 +0000

    Finished submission

--- main.py
+++ main.py
@@ -1,2 +1,44 @@
 # Built-in imports
+def reverse(txt: str) -> str:
+  """
+  Revserses a string
+  
+  Parameter
+  ----------
+  txt: str
+    The string to reverse
 
+  Returns
+  ----------
+  str
+    The reversed string
+  """
+  if txt:
+    return reverse(txt[1:])+txt[0]
+  else:
+    return txt
+
+def is_palindrome(txt: str) -> bool:
+  """
+  checks if the string is a palindrome
+  
+  Parameter
+  ----------
+  txt: str
+    The string to check
+
+  Returns
+  ----------
+  bool
+    whether the string is palindrome
+  """
+  if len(txt)<2:
+    return True
+  txt = txt.strip()
+  txt = txt.replace('.','').replace(',','')
+  if txt[0] == txt[-1]:
+    return is_palindrome(txt[1:-1])
+  else:
+    return False
+
+#print(is_palindrome('hello'))
